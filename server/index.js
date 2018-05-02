c1822767c3e65cb8a386736739df878a8c7ae63c
Franco Victorio <victorio.franco@gmail.com>
2018-05-02 13:50:46 -0300

    Add dev script in root directory

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 
 const app = express()
 
-const port = process.env.PORT || 8000
+app.use(express.static('public'))
+
+const port = process.env.PORT || 3001
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
