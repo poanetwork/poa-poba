b8d85c17c83b7e64281de64675e02dc27c95ac13
Franco Victorio <victorio.franco@gmail.com>
2018-05-08 16:54:35 -0300

    Use pino for logging

--- server/index.js
+++ server/index.js
@@ -6,6 +6,9 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const Web3 = require('web3')
 const plaid = require('plaid')
+const pino = require('pino')
+
+const logger = pino()
 
 const PLAID_CLIENT_ID = process.env.REACT_APP_PLAID_CLIENT_ID
 const PLAID_SECRET = process.env.REACT_APP_PLAID_SECRET
@@ -31,6 +34,7 @@ const privateKey = '0x3daa79a26454a5528a3523f9e6345efdbd636e63f8c24a835204e6ccb5
 const exchangePublicToken = async token => {
   const tokenResponse = await plaidClient.exchangePublicToken(token)
   if (tokenResponse.status_code !== 200) {
+    logger.error({ status: tokenResponse.status_code }, 'Error exchanging public token')
     throw Error(`[exchangePublicToken] ${tokenResponse.error_code}: {tokenResponse.error_message}`)
   }
   return tokenResponse
@@ -39,6 +43,7 @@ const exchangePublicToken = async token => {
 const getBankAccountData = async accessToken => {
   const response = await plaidClient.getAuth(accessToken)
   if (response.status_code !== 200) {
+    logger.error({ status: response.status_code }, 'Error getting bank account')
     throw Error(`[getBankAccountData] ${response.error_code}: {response.error_message}`)
   }
   return response
@@ -46,14 +51,20 @@ const getBankAccountData = async accessToken => {
 
 app.post('/api/get-tx-data', (req, res) => {
   const { ethAccount, token } = req.body
+
+  logger.info({ ethAccount }, 'Getting bank accounts')
+
   return exchangePublicToken(token)
     .then(tokenResponse => {
       const accessToken = tokenResponse.access_token
       return getBankAccountData(accessToken)
     })
     .then(accountData => {
+      logger.info({ accountData }, 'Got account data')
       const { numbers } = accountData
       const bankAccount = numbers[0].account
+
+      logger.info({ bankAccount }, 'Got bank account')
 
       const hash = web3.utils.sha3(ethAccount + Buffer.from(bankAccount).toString('hex'))
       const { v, r, s } = web3.eth.accounts.sign(hash, privateKey)
@@ -61,11 +72,12 @@ app.post('/api/get-tx-data', (req, res) => {
       res.send({ bankAccount, v, r, s })
     })
     .catch(e => {
+      logger.error(e, 'There was an error getting the transaction data')
       res.send({ error: e })
     })
 })
 
 const port = process.env.PORT || 3001
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
+  logger.info(`Listening on port ${port}`)
 })
