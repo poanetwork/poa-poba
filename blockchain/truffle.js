021579b72a2d9aa28bf92df4549d60bc4c08a847
Franco Victorio <victorio.franco@gmail.com>
2018-05-09 14:53:56 -0300

    Add Rinkeby to networks

--- blockchain/truffle.js
+++ blockchain/truffle.js
@@ -1,9 +1,18 @@
+const HDWalletProvider = require('truffle-hdwallet-provider')
+
+const mnemonic = 'gauge pilot return sauce iron woman reason ridge across kangaroo sorry ritual'
+const rinkebyUrl = 'https://rinkeby.infura.io'
+
 module.exports = {
   networks: {
     development: {
       host: '127.0.0.1',
       port: 8545,
       network_id: '*'
+    },
+    rinkeby: {
+      provider: new HDWalletProvider(mnemonic, rinkebyUrl),
+      network_id: 4
     }
   }
 }
