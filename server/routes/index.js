b13b27b51995b350bd854161f0f0d0a2008d1a95
Matias Garat Ortiz <matiasgaratortiz@gmail.com>
2018-05-10 15:52:41 -0300

    Disable eslint-rules

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,6 +10,7 @@ const getRoutes = app => {
     .filter(file => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js')
     .map(file => file.replace('.js', ''))
     .forEach(file => {
+      // eslint-disable-next-line global-require, import/no-dynamic-require
       const route = require(`./${file}`)
       app.use(`/api/${file}`, route)
       logger.info(`registering route /api/${file}`)
