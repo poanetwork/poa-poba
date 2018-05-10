0ec489414de1b714d910b3b3332353a36da81b45
Franco Victorio <franco.victorio@altoros.com>
2018-05-10 12:27:55 -0300

    Add fonts

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { css } from 'glamor'
 import 'font-awesome/css/font-awesome.css'
 import App from './App'
-import globalStyles from './ui/styles/global'
+import './ui/styles/global'
 import registerServiceWorker from './registerServiceWorker'
-
-css.global('*', {
-  boxSizing: 'border-box'
-})
-css.global('html, body', globalStyles)
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
