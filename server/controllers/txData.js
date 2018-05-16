1fdfd5ddbcef9df5f74e355461e892d79cfceb2a
Matias Garat Ortiz <matiasgaratortiz@gmail.com>
2018-05-16 02:14:50 -0300

    Add error handling

--- server/controllers/txData.js
+++ server/controllers/txData.js
@@ -5,7 +5,10 @@ const exchangePublicToken = async token => {
   const tokenResponse = await plaidClient.exchangePublicToken(token)
   if (tokenResponse.status_code !== 200) {
     logger.error({ status: tokenResponse.status_code }, 'Error exchanging public token')
-    throw Error(`[exchangePublicToken] ${tokenResponse.error_code}: {tokenResponse.error_message}`)
+    throw new Error({
+      status: 500,
+      message: `[exchangePublicToken] ${tokenResponse.error_code}: {tokenResponse.error_message}`
+    })
   }
   return tokenResponse
 }
@@ -14,23 +17,34 @@ const getAuth = async accessToken => {
   const response = await plaidClient.getAuth(accessToken)
   if (response.status_code !== 200) {
     logger.error({ status: response.status_code }, 'Error getting bank account')
-    throw Error(`[getAuth] ${response.error_code}: {response.error_message}`)
+    throw new Error({
+      status: 500,
+      message: `[getAuth] ${response.error_code}: {response.error_message}`
+    })
   }
   return response
 }
 
 const getAccessToken = async token => {
-  const tokenResponse = await exchangePublicToken(token)
-  return tokenResponse.access_token
+  try {
+    const tokenResponse = await exchangePublicToken(token)
+    return tokenResponse.access_token
+  } catch (e) {
+    return e
+  }
 }
 
 const getBankAccount = async accessToken => {
-  const accountData = await getAuth(accessToken)
-  logger.info({ accountData }, 'Got account data')
-  const { numbers } = accountData
-  const bankAccount = numbers[0].account
-  logger.info({ bankAccount }, 'Got bank account')
-  return bankAccount
+  try {
+    const accountData = await getAuth(accessToken)
+    logger.info({ accountData }, 'Got account data')
+    const { numbers } = accountData
+    const bankAccount = numbers[0].account
+    logger.info({ bankAccount }, 'Got bank account')
+    return bankAccount
+  } catch (e) {
+    return e
+  }
 }
 
 module.exports = {
--- server/routes/get-tx-data.js
+++ server/routes/get-tx-data.js
@@ -9,20 +9,28 @@ const logger = require('../etc/logger')
 const { PRIVATE_KEY } = process.env
 const web3 = new Web3()
 
-router.post('/', (req, res) => {
+router.post('/', (req, res, next) => {
   const { ethAccount, token } = req.body
   logger.info({ ethAccount }, 'Getting bank accounts')
   const getTxData = async () => {
     try {
+      if (!ethAccount)
+        throw new Error({
+          status: 404,
+          message: 'ETH Account was not found'
+        })
+      if (!token)
+        throw new Error({
+          status: 404,
+          message: 'Token was not found'
+        })
       const accessToken = await txDataController.getAccessToken(token)
       const bankAccount = await txDataController.getBankAccount(accessToken)
       const hash = web3.utils.sha3(ethAccount + Buffer.from(bankAccount).toString('hex'))
       const { v, r, s } = web3.eth.accounts.sign(hash, PRIVATE_KEY)
-
       res.send({ bankAccount, v, r, s })
     } catch (e) {
-      logger.error(e, 'There was an error getting the transaction data')
-      res.send({ error: e })
+      next()
     }
   }
 
