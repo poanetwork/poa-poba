22b88bb6c5c5498d5ecace669e35465773b416e5
Matias Garat Ortiz <matiasgaratortiz@gmail.com>
2018-05-16 16:54:20 -0300

    Update tests

--- server/controllers/txData.js
+++ server/controllers/txData.js
@@ -1,50 +1,27 @@
 const plaidClient = require('../etc/plaid')
 const logger = require('../etc/logger')
 
-const exchangePublicToken = async token => {
+const getAccessToken = async token => {
   const tokenResponse = await plaidClient.exchangePublicToken(token)
   if (tokenResponse.status_code !== 200) {
     logger.error({ status: tokenResponse.status_code }, 'Error exchanging public token')
-    throw new Error({
-      status: 500,
-      message: `[exchangePublicToken] ${tokenResponse.error_code}: {tokenResponse.error_message}`
-    })
+    throw Error(`[exchangePublicToken] ${tokenResponse.error_code}: ${tokenResponse.error_message}`)
   }
-  return tokenResponse
-}
+  return tokenResponse.access_token
 
-const getAuth = async accessToken => {
-  const response = await plaidClient.getAuth(accessToken)
-  if (response.status_code !== 200) {
-    logger.error({ status: response.status_code }, 'Error getting bank account')
-    throw new Error({
-      status: 500,
-      message: `[getAuth] ${response.error_code}: {response.error_message}`
-    })
-  }
-  return response
-}
-
-const getAccessToken = async token => {
-  try {
-    const tokenResponse = await exchangePublicToken(token)
-    return tokenResponse.access_token
-  } catch (e) {
-    return e
-  }
 }
 
 const getBankAccount = async accessToken => {
-  try {
-    const accountData = await getAuth(accessToken)
-    logger.info({ accountData }, 'Got account data')
-    const { numbers } = accountData
-    const bankAccount = numbers[0].account
-    logger.info({ bankAccount }, 'Got bank account')
-    return bankAccount
-  } catch (e) {
-    return e
+  const accountData = await plaidClient.getAuth(accessToken)
+  if (accountData.status_code !== 200) {
+    logger.error({ status: accountData.status_code }, 'Error getting bank account')
+    throw Error(`[getAuth] ${accountData.error_code}: ${accountData.error_message}`)
   }
+  logger.info({ accountData }, 'Got account data')
+  const { numbers } = accountData
+  const bankAccount = numbers[0].account
+  logger.info({ bankAccount }, 'Got bank account')
+  return bankAccount
 }
 
 module.exports = {
--- server/tests/txData.spec.js
+++ server/tests/txData.spec.js
@@ -18,9 +18,21 @@ jest.mock('../etc/plaid', () => ({
 
 describe('txData controller', () => {
   it('should get the access token', async () => {
-    await expect(txDataController.getAccessToken('good-token')).resolves.toBeTruthy()
+    const response = await txDataController.getAccessToken('good-token')
+    expect(response).toEqual(mockExchangePublicTokenResponse.access_token)
+  })
+  it('should return error when getAccessToken fails', async () => {
+    await txDataController.getAccessToken('bad-token').catch(e => {
+      expect(e.message).toBeTruthy()
+    })
   })
   it('should get the bank account', async () => {
-    await expect(txDataController.getBankAccount('good-accessToken')).resolves.toBeTruthy()
+    const account = await txDataController.getBankAccount('good-accessToken')
+    expect(account).toEqual(mockGetAuthResponse.numbers[0].account)
+  })
+  it('should get the bank account', async () => {
+    await txDataController.getAccessToken('bad-accessToken').catch(e => {
+      expect(e.message).toBeTruthy()
+    })
   })
 })
