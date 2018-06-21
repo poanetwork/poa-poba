e453df584042ad9efa5419011be73185ece1c122
Matias Garat Ortiz <matiasgaratortiz@gmail.com>
2018-06-21 15:53:37 -0300

    Add basic tests for UI

---
+++ frontend/src/__tests__/BankAccountList.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { configure, mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import BankAccountList from '../ui/BankAccountList'
+
+configure({ adapter: new Adapter() })
+
+describe('<BankAccountList />', () => {
+  it('renders correctly for 0 bank accounts', () => {
+    const wrapper = mount(<BankAccountList bankAccounts={[]} />)
+
+    expect(wrapper.find('div.bank-account-list')).toHaveLength(1)
+    expect(wrapper.find('div.bank-account-list div.bank-account-item')).toHaveLength(0)
+  })
+
+  it('renders correctly for 1+ bank accounts', () => {
+    const wrapper = mount(<BankAccountList bankAccounts={['bank_account_1', 'bank_account_2']} />)
+
+    expect(wrapper.find('div.bank-account-list')).toHaveLength(1)
+    expect(wrapper.find('div.bank-account-list div.bank-account-item')).toHaveLength(2)
+    expect(wrapper.find('div.bank-account-list button')).toHaveLength(2)
+  })
+})
---
+++ frontend/src/__tests__/Loading.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { configure, mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import Loading from '../ui/Loading'
+
+configure({ adapter: new Adapter() })
+
+describe('<Loading />', () => {
+  it('renders correctly when show is false', () => {
+    const wrapper = mount(<Loading />)
+
+    expect(wrapper.find('div.loading-container')).toHaveLength(0)
+  })
+  it('renders correctly when show is true', () => {
+    const wrapper = mount(<Loading show={true} />)
+
+    expect(wrapper.find('div.loading-container')).toHaveLength(1)
+    expect(wrapper.find('div.loading-container div.loading-inner')).toHaveLength(1)
+    expect(wrapper.find('div.loading-container div.loading-inner div.loading-dot')).toHaveLength(6)
+  })
+})
