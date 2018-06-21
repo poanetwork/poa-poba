ff3a70bedd004975d0f284d7ed12686e7d993abd
Matias Garat Ortiz <matiasgaratortiz@gmail.com>
2018-06-21 15:54:02 -0300

    Add className to UI components

--- frontend/src/ui/RegisteredAccountsList.js
+++ frontend/src/ui/RegisteredAccountsList.js
@@ -3,7 +3,7 @@ import { Div } from 'glamorous'
 
 const RegisteredAccountsList = ({ accounts }) =>
   accounts.length ? (
-    <Div textAlign="left" marginTop="1em">
+    <Div className="registered-accounts-list" textAlign="left" marginTop="1em">
       You have {accounts.length} registered account{accounts.length > 1 ? 's' : ''}:
       <ul>{accounts.map((account, index) => <li key={index}>{account}</li>)}</ul>
     </Div>
