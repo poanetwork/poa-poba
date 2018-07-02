5bfd7588bb7224eac519c42ef37ca0a4489fe312
Matias Garat Ortiz <matiasgaratortiz@gmail.com>
2018-07-02 13:34:29 -0300

    fix linter

--- blockchain/test/poba.js
+++ blockchain/test/poba.js
@@ -2,9 +2,9 @@ const PoBA = artifacts.require('PoBa')
 
 contract('ownership', () => {
   it('signer should be equal to owner', async () => {
-    const poba = await PoBA.deployed();
-    const owner = await poba.owner();
-    const signer = await poba.signer();
+    const poba = await PoBA.deployed()
+    const owner = await poba.owner()
+    const signer = await poba.signer()
     assert.equal(owner, signer)
   })
 })
