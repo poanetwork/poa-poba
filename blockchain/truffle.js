da256e31045f97ae4ad8b21471b6009344205c3e
Matias Garat Ortiz <matiasgaratortiz@gmail.com>
2018-07-04 14:05:38 -0300

    Add coverage and basic test to smart-contracts (#52)
    * Add solidity-coverage package and test and coverage scripts
    
    * Add basic test
    
    * Ignore coverage folder
    
    * Merge coverages
    
    * Update travis script
    
    * fix linter
    
    * Use case-sensitive filename
    
    * Add ethereumjs-abi package
    
    * Reduce coverage values in frontend test
    
    * Reduce coverage values in frontend test

--- blockchain/truffle.js
+++ blockchain/truffle.js
@@ -10,6 +10,19 @@ module.exports = {
       port: 8545,
       network_id: '*'
     },
+    test: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*',
+      gas: '5000000'
+    },
+    coverage: {
+      host: '127.0.0.1',
+      port: 8555,
+      network_id: '*',
+      gas: '0xfffffffffff',
+      gasPrice: 0x01
+    },
     rinkeby: {
       provider: () => new HDWalletProvider(mnemonic, rinkebyUrl),
       network_id: 4
