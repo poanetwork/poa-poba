c55e7271632996e0dab25dab48c5ffdbca8faf02
Jorge Shirai <jlshirai@gmail.com>
2018-08-07 14:58:53 -0300

    Add @TODO for method usage in RegisterAccountsList.

--- frontend/src/ui/RegisteredAccountsList.js
+++ frontend/src/ui/RegisteredAccountsList.js
@@ -1,32 +1,30 @@
 import React from 'react'
 import { Div } from 'glamorous'
 
-const getRegisteredBankAccounts = async () => {
-  try {
-    this.pobaContract = await PobaContract.deployed()
-
-    const registeredAccountsCount = await this.pobaContract.accountsLength.call(account)
-
-    const whenAccounts = []
-    for (let i = 0; i < registeredAccountsCount; i++) {
-      whenAccounts.push(this.pobaContract.getBankAccounts(account, i))
-    }
-
-    const response = await Promise.all(whenAccounts)
-    const registeredAccounts = response.map(account => {
-      return [
-        account[0],
-        account[1],
-        account[2].toString()
-      ]
-    })
-
-    this.setState({ registeredAccounts })
-  } catch (e) {
-    console.error('Contract is not deployed on this network', e)
-    errorAlert('Contract is not deployed on this network')
-  }
-}
+// @TODO: should be used to render the component below
+// const getRegisteredBankAccounts = async account => {
+//   PobaContract.setProvider(this.props.web3.currentProvider)
+//   try {
+//     const pobaContract = await PobaContract.deployed()
+//
+//     const registeredAccountsCount = await pobaContract.accountsLength.call(account)
+//
+//     const whenAccounts = []
+//     for (let i = 0; i < registeredAccountsCount; i++) {
+//       whenAccounts.push(pobaContract.getBankAccounts(account, i))
+//     }
+//
+//     const response = await Promise.all(whenAccounts)
+//     const registeredAccounts = response.map(acc => {
+//       return [acc[0], acc[1], acc[2].toString()]
+//     })
+//
+//     this.setState({ registeredAccounts })
+//   } catch (e) {
+//     console.error('Contract is not deployed on this network', e)
+//     errorAlert('Contract is not deployed on this network')
+//   }
+// }
 
 const RegisteredAccountsList = ({ accounts }) =>
   accounts.length ? (
