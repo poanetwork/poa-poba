b1a264fb9e92afa2f79681a08f4b0b226b3b95b8
Matias Garat Ortiz <matiasgaratortiz@gmail.com>
2018-08-08 12:13:53 -0300

    Update HelpPage

--- frontend/src/ui/pages/HelpPage.js
+++ frontend/src/ui/pages/HelpPage.js
@@ -1,70 +1,39 @@
 import React from 'react'
+import { H1, P } from 'glamorous'
 
-// import BackButton from './BackButton';
+import BackButton from '../containers/BackButton'
 
 const HelpPage = () => {
   return (
-    <div className="content help-page">
-      <div className="col-md-12">
-        <h1 className="main-title">How it works?</h1>
-        <div className="how-to swipe" id="slider">
-          <div className="swipe-wrap">
-            <div className="how-to-i how-to-i_fill-form">
-              <p className="how-to-title">
+    <div>
+      <div>
+        <H1>How it works?</H1>
+        <div>
+          <div>
+            <div>
+              <P style={{fontWeight: 'bold'}}>
                 <span>Step 1: </span>
-                Fill form
-              </p>
-              <p className="how-to-description">
-                Fill the form with your full name and postal address
-              </p>
+                Connect Bank Account
+              </P>
+              <P>Choose the Bank and complete the form with your username and password</P>
             </div>
-            <div className="how-to-i how-to-i_sign-transaction">
-              <p className="how-to-title">
+            <div>
+              <P style={{fontWeight: 'bold'}}>
                 <span>Step 2: </span>
-                Sign transaction
-              </p>
-              <p className="how-to-description">
-                Sign transaction in MetaMask to add your data to smart contract and send you a
-                postcard
-              </p>
+                Choose the bank account
+              </P>
+              <P>Choose one of your bank accounts to verify</P>
             </div>
-            <div className="how-to-i how-to-i_get-postcard">
-              <p className="how-to-title">
+            <div>
+              <P style={{fontWeight: 'bold'}}>
                 <span>Step 3: </span>
-                Get postcard
-              </p>
-              <p className="how-to-description">
-                Check your mailbox for the postcard with confirmation code on it
-              </p>
+                Sign transaction & Finalize proof
+              </P>
+              <P>Sign transaction in MetaMask to add your data to smart contract</P>
             </div>
-            <div className="how-to-i how-to-i_type-code">
-              <p className="how-to-title">
-                <span>Step 4: </span>
-                Type code
-              </p>
-              <p className="how-to-description">
-                Open the webpage specified on the postcard and type in confirmation code
-              </p>
-            </div>
-            <div className="how-to-i how-to-i_finalize-proof">
-              <p className="how-to-title">
-                <span>Step 5: </span>
-                Finalize proof
-              </p>
-              <p className="how-to-description">
-                Sign the second transaction to verify the code and finalize the process
-              </p>
-            </div>
-          </div>
-          <div className="how-to-navigation">
-            <div className="how-to-navigation-i how-to-navigation-i_active" />
-            <div className="how-to-navigation-i" />
-            <div className="how-to-navigation-i" />
-            <div className="how-to-navigation-i" />
-            <div className="how-to-navigation-i" />
           </div>
         </div>
-        {/* <BackButton /> */}
+        <BackButton />
       </div>
     </div>
   )
