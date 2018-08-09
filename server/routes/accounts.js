79448761cd6ec9faf5cd09c3a35ac74795879f3b
Jorge Shirai <jlshirai@gmail.com>
2018-08-09 14:55:13 -0300

    Add endpoint to retrieve single bank account details.

--- server/routes/accounts.js
+++ server/routes/accounts.js
@@ -25,6 +25,22 @@ const getBankAccounts = (req, res) => {
   return bankAccounts(token)
 }
 
+const getSingleBankAccount = (req, res) => {
+  const { token, accountId } = req.params
+  logger.info('Getting single account details')
+  const bankAccounts = async publicToken => {
+    try {
+      const accessToken = await accountsController.getAccessToken(publicToken)
+      const account = await accountsController.getBankAccount(accessToken, accountId)
+      return res.send({ account })
+    } catch (e) {
+      logger.error(e.message, 'There was an error getting the transaction data')
+      return res.status(400).send({ error: e.message })
+    }
+  }
+  return bankAccounts(token)
+}
+
 const signBankAccount = (req, res) => {
   const { ethAccount, token, accountId } = req.body
   logger.info({ ethAccount }, 'Getting bank account')
@@ -52,7 +68,8 @@ const signBankAccount = (req, res) => {
   return getTxData()
 }
 
-router.get('/bank-accounts/:token', getBankAccounts)
 router.post('/sign-account', signBankAccount)
+router.get('/bank-accounts/:token', getBankAccounts)
+router.get('/bank-accounts/:token/:accountId', getSingleBankAccount)
 
 module.exports = router
