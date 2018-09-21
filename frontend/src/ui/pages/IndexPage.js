0c3502adb76451e2c9ab59c4c2210104486f9730
Jorge Luis Shirai <jlshirai@gmail.com>
2018-09-21 11:58:59 -0300

    Fix eslint errors in IndexPage.js.

--- frontend/src/ui/pages/IndexPage.js
+++ frontend/src/ui/pages/IndexPage.js
@@ -32,10 +32,11 @@ class IndexPage extends React.Component {
       <div>
         <H1>Proof of bank account</H1>
         <P className="main">
-          This ĐApp can be used to prove your ownership of a bank account in one of the supported banks.
-          A widget provided by <a href="https://plaid.com/">Plaid</a> is used to verify that you have access to the bank account.
-          Except for blockchain transaction fee to call the smart-contract, there is no extra cost of the verifiaction process.
-          If you have more questions, check out <strong>How it works</strong> section.
+          This ĐApp can be used to prove your ownership of a bank account in one of the supported
+          banks. A widget provided by <a href="https://plaid.com/">Plaid</a> is used to verify that
+          you have access to the bank account. Except for blockchain transaction fee to call the
+          smart-contract, there is no extra cost of the verifiaction process. If you have more
+          questions, check out <strong>How it works</strong> section.
         </P>
         <Link to="/help">
           <button style={buttonStyle}>
@@ -45,9 +46,7 @@ class IndexPage extends React.Component {
         <PlaidButton web3={this.state.web3} account={this.state.accounts[0]} />
 
         <H2>My bank accounts</H2>
-        <P>
-          To view the list of your verified bank accounts click the button below.
-        </P>
+        <P>To view the list of your verified bank accounts click the button below.</P>
         <Link to="/mybankaccountslist">
           <button style={buttonStyle}>
             My Bank Accounts <MyAccountsIcon />
