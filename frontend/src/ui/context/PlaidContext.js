0681c48f9bb70f7eb8f643d229b19405e55ccc62
Jorge Shirai <jlshirai@gmail.com>
2018-11-30 13:13:33 -0300

    Store Plaid token in localStorage.
    Store Plaid token in localStorage so refresh in Plaid bank accounts page 
    is more user friendly.

--- frontend/src/ui/context/PlaidContext.js
+++ frontend/src/ui/context/PlaidContext.js
@@ -5,13 +5,15 @@ const PlaidContext = createContext({})
 export const PlaidContextProvider = PlaidContext.Provider
 export const PlaidContextConsumer = PlaidContext.Consumer
 
+const LOCAL_STORAGE_KEY = 'PlaidToken'
+
 export class PlaidContextManager {
   constructor() {
     this.plaidToken = null
     this.setPlaidToken = token => {
-      this.plaidToken = token
+      localStorage.setItem(LOCAL_STORAGE_KEY, token)
     }
-    this.getPlaidToken = () => this.plaidToken
+    this.getPlaidToken = () => localStorage.getItem(LOCAL_STORAGE_KEY)
   }
 }
 
