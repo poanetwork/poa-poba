f355a861cd3d2b64165925bd49f43c84fe41f4d4
Jorge Luis Shirai <jlshirai@gmail.com>
2018-12-07 12:42:55 -0300

    Add basic issueErc735Claim server side validation.
    Add issueErc735Claim.route.spec.js.

--- server/controllers/issueErc735Claim.js
+++ server/controllers/issueErc735Claim.js
@@ -1,3 +1,4 @@
+const validations = require('../server-lib/validations')
 const getBankAccountDetails = require('../server-lib/get_bank_account_details')
 const erc735Claim = require('../server-lib/erc735_claim')
 
@@ -5,12 +6,30 @@ const logger = require('../etc/logger')
 
 const { POBA_ERC725_CONTRACT_ADDRESS, POBA_ERC725_URI } = process.env
 
-const issueErc735Claim = (req, res) => {
-  const { ethAccount, keccakIdentifier, identityContractAddress } = req.body
+const validateParams = (body, param) => {
+  const result =
+    param === 'ethAccount' || param === 'identityContractAddress'
+      ? validations.validate.wallet(body[param])
+      : validations.validate.string(body[param])
+  if (!result.ok) {
+    const log = `validation error on ${param}: ${body[param]}, err: ${result.msg}`
+    throw new Error(log)
+  }
+}
 
-  logger.info({ ethAccount }, 'Getting bank account from contract')
+const issueErc735Claim = (req, res) => {
   const getTxData = async () => {
     try {
+      if (!req.body || !Object.keys(req.body).length) {
+        throw new Error('request body empty')
+      }
+      validateParams(req.body, 'ethAccount')
+      validateParams(req.body, 'identityContractAddress')
+      validateParams(req.body, 'keccakIdentifier')
+
+      const { ethAccount, keccakIdentifier, identityContractAddress } = req.body
+      logger.info({ ethAccount }, 'Getting bank account from contract')
+
       const bankAccount = await getBankAccountDetails(ethAccount, keccakIdentifier)
       const { signature, bankAccountSha3 } = erc735Claim.getErc735Signature(
         ethAccount,
---
+++ server/tests/issueErc735Claim.route.spec.js
@@ -0,0 +1,86 @@
+const request = require('supertest')
+const app = require('../app')
+
+const ENDPOINT = `/api/issueErc735Claim`
+const validValues = {
+  ethAccount: '0x92970dbd5c0ee6b439422bfd7cd71e1dda921a03',
+  keccakIdentifier: '0x39ae2e5da76434b371b621b266846f5d615d8e5540811c46314341d6259ca8c0',
+  identityContractAddress: '0x92970dbd5c0ee6b439422bfd7cd71e1dda921a03'
+}
+jest.mock('../server-lib/get_bank_account_details', () =>
+  jest.fn(() => {
+    return Promise.resolve({
+      bankName: 'bankName',
+      identityNames: 'identityNames'
+    })
+  })
+)
+jest.mock('../server-lib/erc735_claim', () => ({
+  getErc735Signature: jest.fn(() => {
+    return {
+      signature: 'signature',
+      bankAccountSha3: 'bankAccountSha3'
+    }
+  })
+}))
+describe('[routes] issueErc735Claim', () => {
+  it('should return error if the body of the req is empty', async () => {
+    const emptyBodyRes = await request(app)
+      .post(ENDPOINT)
+      .send({})
+    expect(emptyBodyRes.status).toEqual(400)
+  })
+  it('should return error if ethAccount, keccakIdentifier or identityConractAddress are empty/not present in the body of the req', async () => {
+    const missingEthAccount = await request(app)
+      .post(ENDPOINT)
+      .send({
+        keccakIdentifier: validValues.keccakIdentifier,
+        identityContractAddres: validValues.identityContractAddres
+      })
+    expect(missingEthAccount.status).toEqual(400)
+    const missingKeccakIdentifier = await request(app)
+      .post(ENDPOINT)
+      .send({
+        ethAccount: validValues.ethAccount,
+        identityContractAddres: validValues.identityContractAddres
+      })
+    expect(missingKeccakIdentifier.status).toEqual(400)
+    const missingIdentityContractAddress = await request(app)
+      .post(ENDPOINT)
+      .send({
+        ethAccount: validValues.ethAccount,
+        keccakIdentifier: validValues.keccakIdentifier
+      })
+    expect(missingIdentityContractAddress.status).toEqual(400)
+  })
+  it('should return error if ethAccount or identityConractAddress ar not valid addresses', async () => {
+    const invalidEthAccount = await request(app)
+      .post(ENDPOINT)
+      .send({
+        ethAccount: 'somethingInvalid',
+        keccakIdentifier: validValues.keccakIdentifier,
+        identityContractAddres: validValues.identityContractAddress
+      })
+    expect(invalidEthAccount.status).toEqual(400)
+    const invalidIdentityContractAddress = await request(app)
+      .post(ENDPOINT)
+      .send({
+        ethAccount: validValues.ethAccount,
+        keccakIdentifier: validValues.keccakIdentifier,
+        identityContractAddres: 'somethingInvalid'
+      })
+    expect(invalidIdentityContractAddress.status).toEqual(400)
+  })
+  it('should return signature, data, issuerAddres and uri fields of an erc735 claim on success', () =>
+    request(app)
+      .post(`/api/issueErc735Claim`)
+      .send(validValues)
+      .then(res => {
+        expect(res.status).toEqual(200)
+        const { signature, data, issuerAddress, uri } = res.body
+        expect(signature).toBeTruthy()
+        expect(data).toBeTruthy()
+        expect(issuerAddress).toBeTruthy()
+        expect(uri).toBeTruthy()
+      }))
+})
