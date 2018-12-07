e61aef1bf12a7a3e71bb9805a641dab5948ea6fe
Jorge Shirai <jlshirai@gmail.com>
2018-12-07 14:30:49 -0300

    Remove unused loggin from server-lib/poba_contract.js.

--- server/server-lib/poba_contract.js
+++ server/server-lib/poba_contract.js
@@ -1,10 +1,8 @@
-const logger = require('../etc/logger')
 const truffleContract = require('truffle-contract')
 const Web3 = require('web3')
 const pobaArtifact = require('../artifacts/PoBA.json')
 
 const { RPC } = process.env
-logger.error(RPC)
 const web3 = new Web3(RPC)
 const PoBAContract = truffleContract(pobaArtifact)
 PoBAContract.setProvider(web3.currentProvider)
